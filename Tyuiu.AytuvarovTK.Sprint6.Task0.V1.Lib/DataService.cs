ea8824b7b7f75b892138b335932210ed28b0e61e
Vlad <Vlad@VLAD_X>
2025-11-30 23:05:42 +0500

    Add project files.

---
+++ Tyuiu.AytuvarovTK.Sprint6.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.AytuvarovTK.Sprint6.Task0.V1.Lib
+{
+    public class DataService : ISprint6Task0V1
+    {
+        public double Calculate(int x)
+        {
+            // Дано выражение вычислить его значение при x = 3, результат вывести в TextBox. Округлить до трёх знаков после запятой. Графический интерфейс оформить по шаблону из лекции. y(x) = x / (x^3 + 2) 
+
+            double y = x / (Math.Pow(x, 3) + 2);
+            return Math.Round(y, 3);
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint6.Task0.V1.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.AytuvarovTK.Sprint6.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
