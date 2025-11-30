9eb3bbc909a0d920f563cb5465fb2f6b542af7ac
Vlad <Vlad@VLAD_X>
2025-11-30 23:14:52 +0500

    Task33

---
+++ Tyuiu.AytuvarovTK.Sprint6.Task3.V18.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6; 
+namespace Tyuiu.AytuvarovTK.Sprint6.Task3.V18.Lib
+{
+    public class DataService : ISprint6Task3V18
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            // Дан массив 5 на 5 элементов. Заменить четные значения в четвертой строке на 0. Результат вывести в DataGridView. Графический интерфейс оформить по шаблону из лекции. -19 -19   1  18   7 5   3  -4  -6 -12  -15   6   2   2 -14   -9 -10  15  -5  -6  -13 -15  -9   7   1
+            
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[3, j] % 2 == 0)
+                {
+                    matrix[3, j] = 0;
+                }
+            }
+            return matrix;
+
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint6.Task3.V18/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.AytuvarovTK.Sprint6.Task3.V18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
